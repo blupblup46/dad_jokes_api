0d58e86007848aec9469682e7291b1d2a840c5d4
Louan Robert <louan46@gmail.com>
2024-12-15 11:06:47 +0100

    Api base ready

--- server/main.go
+++ server/main.go
@@ -5,6 +5,7 @@ import (
 	"io"
 	"log"
 	"net/http"
+	"os"
 	"server/utils"
 )
 
@@ -12,10 +13,10 @@ const EXPOSED_PORT = "8080"
 const API_ROOT_URL = "https://icanhazdadjoke.com"
 const API_BATCH_SUFFIX = "/search"
 
-var jokes map[int]string
+var jokes map[string]string
 
 func main() {
-	buildDadabase()
+	go buildDadabase()
 
 	log.Print("Listening on :", EXPOSED_PORT)
 	err := http.ListenAndServe(":"+EXPOSED_PORT, nil)
@@ -25,19 +26,46 @@ func main() {
 
 }
 
-func buildDadabase() {
-	log.Print("calling API")
+func buildDadabase(){
+	jokes = utils.Merge(fetchApi(), getCustomJokes())
+	log.Println("Dadabase ready")
+}
 
+func getCustomJokes() map[string]string {
+	var customJokes utils.CustomJokes
+
+	customJokesFile, err_openCustomJokesFile := os.Open("./jokes/customJokes.json")
+	if  err_openCustomJokesFile != nil{
+		log.Fatal("Could not open custom jokes file", err_openCustomJokesFile)
+	}
+
+	customJokesByte, err_readCustomJokesFile := io.ReadAll(customJokesFile)
+	if  err_readCustomJokesFile != nil{
+		log.Fatal("Could not read custom jokes file", err_readCustomJokesFile)
+	}
+
+	err_deserializeCustomJokes := json.Unmarshal(customJokesByte, &customJokes)
+	if  err_deserializeCustomJokes != nil{
+		log.Fatal("Could not deserialize custom jokes file", err_deserializeCustomJokes)
+	}
+
+	return utils.ToJokesMap(customJokes.Jokes, false)
+}
+
+func fetchApi() map[string]string {
 	client := &http.Client{}
+	defer client.CloseIdleConnections()
+	
+
 	req, _ := http.NewRequest("GET", API_ROOT_URL+API_BATCH_SUFFIX, nil)
 	req.Header.Set("Accept", "application/json")
 	resp, err := client.Do(req)
-
 	if err != nil {
 		log.Fatal("Could not fetch the API: ", err)
 	}
 
 	body, err := io.ReadAll(resp.Body)
+	defer resp.Body.Close()
 	if err != nil {
 		log.Fatal("Could not read response from API: ", err)
 	}
@@ -47,7 +75,14 @@ func buildDadabase() {
 		log.Fatal("Could not deserialize JSON: ", err)
 	}
 
-	jokes = utils.ToJokesMap(response.Results)
-	log.Print(jokes)
+	jokes = utils.ToJokesMap(response.Results, true)
 
+	JsonApiJokes, err_serializeApiJokes := json.Marshal(jokes)
+	APIJokesFile, err_createFile := os.Create("./jokes/API_jokes.json")
+
+	if err_createFile == nil && err_serializeApiJokes == nil{
+		APIJokesFile.Write(JsonApiJokes)
+	}
+
+	return jokes
 }
--- server/utils/joke.go
+++ server/utils/joke.go
@@ -1,19 +1,36 @@
 package utils
 
 type BatchResponse struct {
-    Results      []Joke  `json:"results"`
+	Results []Joke `json:"results"`
+}
+
+type CustomJokes struct {
+	Jokes []Joke `json:"jokes"`
 }
 
 type Joke struct {
-    ID   string `json:"id"`
-    Joke string `json:"joke"`
+	ID   string `json:"id"`
+	Joke string `json:"joke"`
 }
 
-func ToJokesMap(jokesArr []Joke) map[int]string{
-	res := make(map[int]string)
-	
-	for index, joke := range jokesArr{
-		res[index] = joke.Joke
+func ToJokesMap(jokesArr []Joke, isFromApi bool) map[string]string {
+	res := make(map[string]string)
+	var prefix string
+
+	if isFromApi {
+		prefix = "API_"
+	}
+
+	for _, joke := range jokesArr {
+		res[prefix+joke.ID] = joke.Joke
 	}
 	return res
+}
+
+func Merge(jokes1 map[string]string, jokes2 map[string]string) map[string]string {
+	for k, v := range jokes2 {
+		jokes1[k] = v
+	}
+
+	return jokes1
 }
