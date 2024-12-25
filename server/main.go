ce9c4bc3f1884c561f137426b3549602f54e9d32
Louan Robert <louan46@gmail.com>
2024-12-25 14:17:20 +0100

    update log methodology in server

--- server/main.go
+++ server/main.go
@@ -17,6 +17,7 @@ const API_JOKES_PATH = "./jokes/apiJokes.json"
 const CUSTOM_JOKES_PATH = "./jokes/customJokes.json"
 
 var jokes map[int]utils.Joke
+var logErr = log.New(os.Stderr, "", 0)
 
 func main() {
 	log.Println(`
@@ -64,13 +65,13 @@ func BuildHandlers(muxServer *http.ServeMux) {
 		jData, _ := json.Marshal(joke)
 		w.Header().Set("Content-Type", "application/json")
 		if _, err := w.Write(jData); err != nil {
-			log.Println("Could not send response", err)
+			logErr.Println("Could not send response", err)
 		}
 	}
 
 	muxServer.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
 		if _, err := w.Write([]byte("Up")); err != nil {
-			log.Println("Could not send response", err)
+			logErr.Println("Could not send response", err)
 		}
 	})
 
@@ -99,7 +100,7 @@ func BuildHandlers(muxServer *http.ServeMux) {
 	muxServer.HandleFunc("/reset", func(w http.ResponseWriter, r *http.Request) {
 		buildDadabase(true)
 		if _, err := w.Write([]byte("Dadabase ready !")); err != nil {
-			log.Println("Could not send response", err)
+			logErr.Println("Could not send response", err)
 			w.WriteHeader(http.StatusInternalServerError)
 		}
 	})
@@ -108,7 +109,7 @@ func BuildHandlers(muxServer *http.ServeMux) {
 		jData, _ := json.Marshal(jokes)
 		w.Header().Set("Content-Type", "application/json")
 		if _, err := w.Write(jData); err != nil {
-			log.Println("Could not send response", err)
+			logErr.Println("Could not send response", err)
 		}
 	})
 }
@@ -143,17 +144,17 @@ func GetJokesFromFile(path string) []utils.Joke {
 
 	customJokesFile, err_openCustomJokesFile := os.Open(path)
 	if err_openCustomJokesFile != nil {
-		log.Fatal("Could not open file", err_openCustomJokesFile)
+		logErr.Println("Could not open file", err_openCustomJokesFile)
 	}
 
 	customJokesByte, err_readCustomJokesFile := io.ReadAll(customJokesFile)
 	if err_readCustomJokesFile != nil {
-		log.Fatal("Could not read file", err_readCustomJokesFile)
+		logErr.Println("Could not read file", err_readCustomJokesFile)
 	}
 
 	err_deserializeCustomJokes := json.Unmarshal(customJokesByte, &jokesArr)
 	if err_deserializeCustomJokes != nil {
-		log.Fatal("Could not deserialize file", err_deserializeCustomJokes)
+		logErr.Println("Could not deserialize file", err_deserializeCustomJokes)
 	}
 
 	return jokesArr
@@ -171,34 +172,34 @@ func fetchApi() map[int]utils.Joke {
 	req.Header.Set("Accept", "application/json")
 	resp, err := client.Do(req)
 	if err != nil {
-		log.Fatal("Could not fetch the API: ", err)
+		logErr.Println("Could not fetch the API: ", err)
 	}
 
 	body, err := io.ReadAll(resp.Body)
 	defer resp.Body.Close()
 	if err != nil {
-		log.Fatal("Could not read response from API: ", err)
+		logErr.Println("Could not read response from API: ", err)
 	}
 
 	var response utils.BatchResponse
 	if err := json.Unmarshal(body, &response); err != nil {
-		log.Fatal("Could not deserialize JSON: ", err)
+		logErr.Println("Could not deserialize JSON: ", err)
 	}
 
 	jokesArr := response.Results
 
 	JsonApiJokes, err_serializeApiJokes := json.Marshal(jokesArr)
 	if err_serializeApiJokes != nil {
-		log.Println("Could not serialize API jokes", err_serializeApiJokes)
+		logErr.Println("Could not serialize API jokes", err_serializeApiJokes)
 	}
 
 	APIJokesFile, err_createFile := os.Create(API_JOKES_PATH)
 	if err_createFile != nil {
-		log.Println("Could not create API jokes file", err_createFile)
+		logErr.Println("Could not create API jokes file", err_createFile)
 	}
 
 	if _, err_writeFile := APIJokesFile.Write(JsonApiJokes); err != nil {
-		log.Println("Could not write API jokes file", err_writeFile)
+		logErr.Println("Could not write API jokes file", err_writeFile)
 	}
 
 	return utils.ToMap(jokesArr)
